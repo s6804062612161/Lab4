1e92438eb0f9cb598da4e6a92713e3efffb027a2
s6804062612161 PIYADA BUTSRIPHUM <s6804062612161@email.kmutnb.ac.th>
2025-12-22 11:43:57 +0700

    nested loop

---
+++ lab4no4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (void){
+	int i=1;
+	while (i <= 3){
+		printf("Row %d: ", i);
+		for (int j = 1; j<= 5; j++)
+			printf("%3d", j);
+		printf("\n");
+		i++;
+	}
+	return 0;
+} 
